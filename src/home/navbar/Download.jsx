e12780ca58a06b583d5d58e16ba30f6c30ce8fbc
Sanjay Sharma <sanjayupmanew512@gmail.com>
2025-12-01 13:20:11 +0530

    fix - resume download

--- src/home/navbar/Download.jsx
+++ src/home/navbar/Download.jsx
@@ -3,9 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 import AppButton from "../../components/AppButton.jsx";
 
+
 function Download() {
   return (
-    <a href="/resume-sanjay_sharma.pdf" download>
+    <a href="/portfolio/resume-sanjay_sharma.pdf" download>
       <AppButton className="flex items-center justify-center !py-1 !px-2">
         <FontAwesomeIcon icon={faDownload} size="sm" />
         <span className="ml-2">Resume</span>
